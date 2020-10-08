98981da136b8bbcce9d4c812f00a9007d08d34d9
jagdeepsgill888 <59809683+jagdeepsgill888@users.noreply.github.com>
2020-10-08 15:04:03 -0400

    learning ajax end of class week3

--- js/main.js
+++ js/main.js
@@ -3,7 +3,43 @@ import Team from "./modules/DataModule.js";
 //import Person from "./modules/TheNavSystem.js";
 
 (() => {
+
 // stub * just a place for non-component-specifie stuff
+
+// set up the XMLHttp object
+let myReq = new XMLHttpRequest;
+
+// make sure we can handle whaever data comes back, or any errors
+myReq.addEventListener("readystatechange", handleRequest);
+
+// open a request and pass thruthe URL of the data that we want
+myReq.open('GET', '../DataSet.json');
+
+// actually make the request
+ myReq.send();
+
+// handleRequest funtion goes here
+function handleRequest() {
+    if (myReq.readyState === XMLHttpRequest.DONE) {
+        // check status here and proceed
+        if (myReq.status === 200) {
+            // 200 means done and dusted, ready to go with the dataset!
+            handleDataSet(myReq.responseText);
+
+        } else {
+            // probably got some kind of error code, so handle that 
+            // a 404, 500 etc... can render appropriate error messages here
+            console.error(`${myReq.status} : something done broke, son`);
+        }
+    } else {
+        // request isn't ready yet, keep waiting...
+        console.log(`Request state: ${myReq.readyState}. Still processing...`);
+    }
+
+}
+
+
+
 console.log('Loaded');
 
 // console.log(Team);
@@ -20,12 +56,16 @@ let userSection =document.querySelector(".user-section"),
 function handleDataSet(data) {
     // let currentUser = userTemplate.cloneNode(true),
     //   currentUserText = currentUser.querySelector('.user').children;
+    
+    debugger;
 
     //make a copy of our template here and then
     // populate the children (text element) with
     //the static data from the team object
    
       for (let user in data) {
+
+        debugger;
 
         let currentUser = userTemplate.cloneNode(true),
         currentUserText = currentUser.querySelector('.user').children;
