8d245998a4600fabbdcf53659196c17b2f4e2512
jagdeepsgill888 <59809683+jagdeepsgill888@users.noreply.github.com>
2020-10-22 13:21:36 -0400

    added images to data and done fetch call

--- js/main.js
+++ js/main.js
@@ -4,48 +4,26 @@ import Team from "./modules/DataModule.js";
 
 (() => {
 
-// stub * just a place for non-component-specifie stuff
+    fetch('./DataSet.json')
+    .then(res => res.json()) // parse the JASON (translate) back to Plain
+    .then(data => {
+        // this is our data (DataSet.json)
+        // Converted to a plain JavaScript Object
+        
+    
+        // debugger;
+        console.log(data)
+        handleDataSet(data);
+    // here's where you would call the function that puts the pieces on the page
+    })
 
-// set up the XMLHttp object
-let myReq = new XMLHttpRequest;
-
-// make sure we can handle whaever data comes back, or any errors
-myReq.addEventListener("readystatechange", handleRequest);
-
-// open a request and pass thruthe URL of the data that we want
-myReq.open('GET', '../DataSet.json');
-
-// actually make the request
- myReq.send();
-
-// handleRequest funtion goes here
-function handleRequest() {
-    if (myReq.readyState === XMLHttpRequest.DONE) {
-        // check status here and proceed
-        if (myReq.status === 200) {
-            // 200 means done and dusted, ready to go with the dataset!
-            handleDataSet(JSON.parse(myReq.responseText));
-
-        } else {
-            // probably got some kind of error code, so handle that 
-            // a 404, 500 etc... can render appropriate error messages here
-            console.error(`${myReq.status} : something done broke, son`);
-        }
-    } else {
-        // request isn't ready yet, keep waiting...
-        console.log(`Request state: ${myReq.readyState}. Still processing...`);
-    }
-
-}
-
-
+    .catch((err) => {
+    console.log(err);
+    })
 
 console.log('Loaded');
 
 // console.log(Team);
-
-let userSection =document.querySelector(".user-section"),
- userTemplate = document.querySelector("#profs-template").content;
 
 // debugger;
 
@@ -57,20 +35,22 @@ function handleDataSet(data) {
     // let currentUser = userTemplate.cloneNode(true),
     //   currentUserText = currentUser.querySelector('.user').children;
     
-    debugger;
+    let userSection =document.querySelector(".user-section"),
+    userTemplate = document.querySelector("#profs-template").content;
 
     //make a copy of our template here and then
     // populate the children (text element) with
     //the static data from the team object
-   
+      debugger;
       for (let user in data) {
 
-        debugger;
+        
 
         let currentUser = userTemplate.cloneNode(true),
         currentUserText = currentUser.querySelector('.user').children;
 
              currentUserText[1].textContent = data[user].name;
+             currentUserText[2].src = `images/${data[user].avatar}.jpg`;
              currentUserText[2].textContent = data[user].role;
              currentUserText[3].textContent = data[user].nickname;
 
@@ -78,7 +58,7 @@ function handleDataSet(data) {
       }
    
 
-
+      console.log(data);
  }
   
  //handleDataSet(Team);
